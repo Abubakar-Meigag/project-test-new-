cbb3ab720c57ffd27fab62cdb65201037a329a43
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-27 01:38:27 +0000

    update

--- backend/index.js
+++ backend/index.js
@@ -33,27 +33,50 @@ app.get("/", (req, res) => {
   res.send(`Welcome to test Project server on port  ${port}`);
 });
 
+// app.post("/api/storeUser", async (req, res) => {
+//   try {
+//     const { name, email, picture, sub } = req.body;
+
+//     const insertQuery =
+//       "INSERT INTO users (name, email, picture, sub) VALUES ($1, $2, $3, $4) RETURNING *";
+
+//     const result = await pool.query(insertQuery, [name, email, picture, sub]);
+
+//     res.status(201).json(result.rows[0]);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
 app.post("/api/storeUser", async (req, res) => {
   try {
     const { name, email, picture, sub } = req.body;
     const existingUserQuery = "SELECT * FROM users WHERE email = $1";
     const existingUserResult = await pool.query(existingUserQuery, [email]);
     const existingUser = existingUserResult.rows[0];
-    
-      if (existingUser) {
-        console.log("User already exists. You might want to update the existing record.");
-        return res.status(400).send("User already exists.");
-      }
-    
+
+    if (existingUser) {
+      console.log(
+        "User already exists. You might want to update the existing record."
+      );
+      return res.status(400).send("User already exists.");
+    }
+
+    if (!name || !email || !picture || !sub) {
+      return res
+        .status(400)
+        .json({ error: "Invalid data format or user already exists." });
+    }
+
     const insertQuery =
       "INSERT INTO users (name, email, picture, sub) VALUES ($1, $2, $3, $4) RETURNING *";
 
-    const result = await pool.query(insertQuery,
-      [ name, email, picture, sub ]);
+    const result = await pool.query(insertQuery, [name, email, picture, sub]);
 
     res.status(201).json(result.rows[0]);
   } catch (error) {
-    console.error(error);
+    console.log(error.detail);
     res.status(500).send("Internal Server Error");
   }
 });
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,52 +1,54 @@
-import React from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
-const storeUserInfo = async (user) => {
-  try {
-    if (user && user.given_name) {
-      const response = await axios.post(
-        "http://localhost:5999/api/storeUser",{
-            name: user.name,
-            email: user.email,
-            picture: user.picture,
-            sub: user.sub,
-        },{ headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+  const storeUserInfo = async () => {
+    try {
+        if (user) {
+            const userData = {
+                name: user.given_name || user.name || "DefaultName",
+                email: user.email || "DefaultEmail@example.com",
+                picture: user.picture || "default-profile-picture-url",
+                sub: user.sub,
+            };
 
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+            const response = await axios.post(
+                "http://localhost:5999/api/storeUser",
+                userData, {
+                    headers: {
+                    "Content-Type": "application/json",
+                    },
+                }
+        );
+
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
         const data = response.data;
         console.log(data);
-        
-    } else {
-      console.log("user need to login");
+      } else {
+        console.log("Error storing user: 'user' is undefined");
+      }
+    } catch (error) {
+      console.log("Error storing user:", error);
     }
-  } catch (error) {
-    console.error("Error storing user:", error);
-  }
-};
+  };
+    
+     storeUserInfo();
 
-storeUserInfo();
-
-    return (
-        <div className="App">
-            <h3>user is {isAuthenticated ? "Logged in" : "Not logged in"}</h3>
-            {isAuthenticated && (
-                <pre style={{ textAlign: "start" }}>
-                {JSON.stringify(user, null, 2)}
-                </pre>
-            )}
-        </div>
-    );
+  return (
+    <div className="App">
+      <h3>user is {isAuthenticated ? "Logged in" : "Not logged in"}</h3>
+      {isAuthenticated && (
+        <pre style={{ textAlign: "start" }}>
+          {JSON.stringify(user, null, 2)}
+        </pre>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
